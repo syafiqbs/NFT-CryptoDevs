ce36578b3857a0fac6348d855e95d2f54c2a57a6
syafiqbs <cross.symbol@hotmail.com>
2022-06-15 14:53:09 +0800

    Move project into cloned repo

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x2bA33203b091284974927D336a8f380089b2C7ea";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
